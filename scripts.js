2ae0856e3ec1f17b4d374d0074277fc51afb30c5
Attila Andrade <attilaandrade@gmail.com>
2024-06-28 15:22:38 -0300

    ajuste botão download

--- scripts.js
+++ scripts.js
@@ -1,25 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('register-btn').addEventListener('click', () => {
-        alert('Redirecionando para a página de cadastro...');
-        window.location.href = 'cadastro.html';
-    });
-
-    document.getElementById('download-btn').addEventListener('click', () => {
-        alert('Iniciando download do ebook...');
-        window.location.href = 'ebook.pdf';
-    });
-
-    document.getElementById('contact-form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert('Cadastro realizado com sucesso, boa sorte!');
-    });
-});
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('register-btn').addEventListener('click', () => {
-        alert('Redirecionando para a página de cadastro...');
-        window.location.href = 'cadastro.html';
-    });
-
     document.getElementById('download-btn').addEventListener('click', () => {
         alert('Iniciando download do ebook...');
         window.location.href = 'ebook.pdf';
